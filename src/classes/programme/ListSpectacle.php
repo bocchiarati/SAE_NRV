d12c96e34bd6df750e776de01000e3860f908df8
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2024-11-06 17:37:52 +0100

    Type des retours

--- src/classes/programme/ListSpectacle.php
+++ src/classes/programme/ListSpectacle.php
@@ -4,22 +4,22 @@ namespace iutnc\nrv\programme;
 
 abstract class ListSpectacle implements \Iterator{
     protected array $spectacles = [];
-    public function current() {
+    public function current(): Spectacle {
         return $this->tab[$this->position];    }
 
-    public function next() {
+    public function next(): void {
         ++$this->position;
     }
 
-    public function key() {
+    public function key(): int {
         return $this->position;
     }
 
-    public function valid() {
+    public function valid(): bool {
         return isset($this->tab[$this->position]);
     }
 
-    public function rewind() {
+    public function rewind(): void {
         $this->position = 0;
     }
 }
