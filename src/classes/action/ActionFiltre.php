a4d15b3dcc0607d5c0c57356e7847ba1e0f1573a
ClemtMe <clement.merlin4@etu.univ-lorraine.fr>
2024-11-07 17:50:12 +0100

    change le style des filtres

--- src/classes/action/ActionFiltre.php
+++ src/classes/action/ActionFiltre.php
@@ -12,24 +12,20 @@ class ActionFiltre extends Action {
     {
         $affichage = "";
 
-        if (isset($_GET['id'])) {
-            $pdo = NrvRepository::getInstance();
-            $spectacles = $pdo->SpectaclesByStyle($_GET['id']);
+        if(isset($_GET['filter']) && isset($_GET['id'])){
 
-            $render = new ListSpectacleRenderer($spectacles);
-            return $render->render(Renderer::LONG);
-        } else {
+            if($_GET['filter'] === "style") {
+                //FILTRE STYLE
 
-            $choix = '';
+                $pdo = NrvRepository::getInstance();
+                $spectacles = $pdo->SpectaclesByStyle($_GET['id']);
 
-            $pdo = NrvRepository::getInstance();
-            $listStyle = $pdo->getAllStyle();
-
-            foreach ($listStyle as $key => $style) {
-                $choix .= '<a href="?action=filterByStyle&id=' . $key . '">' . $style . '</a>';
+                $render = new ListSpectacleRenderer($spectacles);
+                $output = $render->render(Renderer::LONG);
             }
 
-            if (isset($_GET['id'])) {
+            if($_GET['filter'] === "location") {
+                //FILTRE LOCATION
                 $selectedLocation = $_GET['id'];
 
                 $repository = NrvRepository::getInstance();
@@ -55,126 +51,145 @@ class ActionFiltre extends Action {
                     }
                 }
                 $output .= "</ul>";
-                return $output;
-            } else {
+            }
+        } else {
+            //FILTRE PAR DEFAUT
+            $output = "Filtre par défaut";
+        }
 
-                $repository = NrvRepository::getInstance();
-                $locations = $repository->getAllLieu();
+        $choix = '';
 
-                // creation des options pour le select du lieu
-                $options = '';
-                foreach ($locations as $lieuID => $nom) {
-                    $options .= "<a href='?action=filterByLocation&id={$lieuID}'>{$nom}</a>";
-                }
+        $pdo = NrvRepository::getInstance();
+        $listStyle = $pdo->getAllStyle();
 
-                $affichage .= <<<HTML
-                    <style>
-                    .dropdown {
-                        position: relative;
-                        display: inline-block;
-                    }
-                    .dropdown-button {
-                        background-color: #FF0000;
-                        color: white;
-                        padding: 10px;
-                        font-size: 16px;
-                        border: none;
-                        cursor: pointer;
-                    }
-                    .dropdown-content {
-                        display: none;
-                        position: absolute;
-                        background-color: #f9f9f9;
-                        min-width: 160px;
-                        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-                        z-index: 1;
-                    }
-                    .dropdown-content a {
-                        color: black;
-                        padding: 12px 16px;
-                        text-decoration: none;
-                        display: block;
-                    }
-                    .dropdown-content a:hover {
-                        background-color: #f1f1f1;
-                    }
-                    .dropdown:hover .dropdown-content {
-                        display: block;
-                    }
-                    .dropdown:hover .dropdown-button {
-                        background-color: #ffcccc;
-                        color: red;
-                    }
-                    /* Container for the form */
-                    form {
-                        display: flex;
-                        flex-direction: column;
-                        font-family: Arial, sans-serif;
-                        width: 170px;
-                    }
-                
-                    /* Input date styling */
-                    #date {
-                        width: 100%;
-                        padding: 10px;
-                        margin: 10px 0;
-                        border-radius: 4px;
-                        border: 1px solid #ccc;
-                        font-size: 14px;
-                        box-sizing: border-box;
-                    }
-                
-                    /* Button styling */
-                    button {
-                        background-color: #ff0000;
-                        color: white;
-                        padding: 10px 20px;
-                        border: none;
-                        border-radius: 4px;
-                        cursor: pointer;
-                        font-size: 16px;
-                        transition: background-color 0.3s ease;
-                    }
-                
-                    /* Button hover effect */
-                    button:hover {
-                        background-color: #ff0000;
-                    }
-                
-                    /* Focus effect on input */
-                    #date:focus {
-                        border-color: #ff0000;
-                        outline: none;
-                    }
-                </style>
-                <div class="filtres">
-                    <h2>Filtrer par style</h2>
-                    <div class="dropdown">
-                        <button class="dropdown-button">Sélectionner un style</button>
-                        <div class="dropdown-content">
-                            $choix
-                        </div>
-                    </div>
-                    
-                    <h2>Filtrer par date</h2>
-                    <form method="post" action="?action=filterByDate">
-                        <input type="date" id="date" name="date" required>
-                        <button type="submit">Filtrer</button>
-                    </form>
-                    
-                    <h2>Filtrer par lieu</h2>
-                    <div class="dropdown">
-                        <button class="dropdown-button">Sélectionner un lieu</button>
-                        <div class="dropdown-content">
-                            $options
-                        </div>
+        foreach ($listStyle as $key => $style) {
+            $choix .= '<a href="?action=filtre&filter=style&id=' . $key . '">' . $style . '</a>';
+        }
+
+        $repository = NrvRepository::getInstance();
+        $locations = $repository->getAllLieu();
+
+        // creation des options pour le select du lieu
+        $options = '';
+        foreach ($locations as $lieuID => $nom) {
+            $options .= "<a href='?action=filtre&filter=location&id={$lieuID}'>{$nom}</a>";
+        }
+
+        $affichage .= <<<HTML
+            <style>
+            .dropdown {
+                position: relative;
+                display: inline-block;
+            }
+            .dropdown-button {
+                background-color: #FF0000;
+                color: white;
+                padding: 10px;
+                font-size: 16px;
+                border: none;
+                cursor: pointer;
+            }
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1;
+            }
+            .dropdown-content a {
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+            }
+            .dropdown-content a:hover {
+                background-color: #f1f1f1;
+            }
+            .dropdown:hover .dropdown-content {
+                display: block;
+            }
+            .dropdown:hover .dropdown-button {
+                background-color: #ffcccc;
+                color: red;
+            }
+            /* Container for the form */
+            form {
+                display: flex;
+                flex-direction: column;
+                font-family: Arial, sans-serif;
+                width: 170px;
+            }
+        
+            /* Input date styling */
+            #date {
+                width: 100%;
+                padding: 10px;
+                margin: 10px 0;
+                border-radius: 4px;
+                border: 1px solid #ccc;
+                font-size: 14px;
+                box-sizing: border-box;
+            }
+        
+            /* Button styling */
+            button {
+                background-color: #ff0000;
+                color: white;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+                font-size: 16px;
+                transition: background-color 0.3s ease;
+            }
+        
+            /* Button hover effect */
+            button:hover {
+                background-color: #ff0000;
+            }
+        
+            /* Focus effect on input */
+            #date:focus {
+                border-color: #ff0000;
+                outline: none;
+            }
+            .container {
+                display: flex;
+                flex-direction: column;
+            }
+        </style>
+        <div class="container">
+            <div class="filtres">
+                <h2>Filtrer par style</h2>
+                <div class="dropdown">
+                    <button class="dropdown-button">Sélectionner un style</button>
+                    <div class="dropdown-content">
+                        $choix
                     </div>
                 </div>
-                HTML;
+                
+                <h2>Filtrer par date</h2>
+                <form method="post" action="?action=filtre">
+                    <input type="date" id="date" name="date" required>
+                    <button type="submit">Filtrer</button>
+                </form>
+                
+                <h2>Filtrer par lieu</h2>
+                <div class="dropdown">
+                    <button class="dropdown-button">Sélectionner un lieu</button>
+                    <div class="dropdown-content">
+                        $options
+                    </div>
+                </div>
+            </div>
+        </div>
+         <div class="affichage">
+            {$output}
+        </div>
+        HTML;
 
-                return $affichage;
-            }
-        }
+        return $affichage;
     }
 
     function executePost(): string
