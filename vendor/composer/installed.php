96c1789674bfec92261b0140a57fed96a50525b4
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-11-09 03:12:39 +0100

    ajout bootstrap

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 's3/nrv',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'deee1b883ba04d5cf33922afb6154b7c15bdb43a',
+        'reference' => '9afa3177b36e603629f21a155b35a297136cb349',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,11 +13,26 @@
         's3/nrv' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'deee1b883ba04d5cf33922afb6154b7c15bdb43a',
+            'reference' => '9afa3177b36e603629f21a155b35a297136cb349',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.3.3',
+            'version' => '5.3.3.0',
+            'reference' => '6e1f75f420f68e1d52733b8e407fc7c3766c9dba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.3.3',
+            ),
         ),
     ),
 );
